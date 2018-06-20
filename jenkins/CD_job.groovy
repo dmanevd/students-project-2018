c0f728c2309dff893605d9abe04ca495543157f9
Dmitrii Manev <dmanevd@mail.ru>
2018-06-20 17:52:10 +0300

    1

--- jenkins/CD_job.groovy
+++ jenkins/CD_job.groovy
@@ -17,49 +17,52 @@ node {
             sh "docker image prune -f"
             sh "docker stop $CONTAINER_NAME"
             sh "docker rm $CONTAINER_NAME"
-          } catch (Exception err) { 
+          } catch (Exception err) {
         }
     }
 
     stage('Running and Integration tests') {
-        withCredentials([usernamePassword(credentialsId: 'postgresql-dmanev', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
-            sh "docker pull $DOCKER_HUB_USER/$CONTAINER_NAME:${env.git_tags}"
-            sh "docker run -d --network greetings_application_default  -p $APP_PORT:$APP_PORT -e 'DB_URL=postgresql://$USER:$PASSWORD@postgresql:$DB_PORT/$DB_NAME' --name $CONTAINER_NAME $DOCKER_HUB_USER/$CONTAINER_NAME:${env.git_tags}"
-        }
-        status = sh(returnStdout: true, script: "docker inspect --format='{{.State.Status}}' $CONTAINER_NAME").trim()
-        if (status != 'running'){
-            currentBuild.result = 'FAILED'
-            sh "exit 1"
-        }
-        sleep 5
-        APP_IP_ADDR = sh(returnStdout: true, script: "docker inspect $CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
-        curl_status_app = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_IP_ADDR:$APP_PORT 1>/dev/null && echo \$?")
-        if (curl_status_app != '0') {
-            currentBuild.result = 'FAILED'
-            sh "exit ${curl_status_app}"
-        }
-        sleep 5
-        grep_status_app = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_IP_ADDR:$APP_PORT | grep 'Greetings, stranger!'").trim()
-        if (grep_status_app != '<title>Greetings, stranger!</title>') {
-            currentBuild.result = 'FAILED'
-            sh "exit ${grep_status_app}"
-        }
-        sleep 5
-        PROXY_IP_ADDR = sh(returnStdout: true, script: "docker inspect $PROXY_CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
-        curl_status_pr = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$PROXY_IP_ADDR:$APP_HTTP_PORT 1>/dev/null && echo \$?")
-        sh(returnStatus: true, script: "echo http://$APP_IP_ADDR:$APP_PORT")
-        if (curl_status_pr != '0') {
-            currentBuild.result = 'FAILED'
-            sh "exit ${curl_status_pr}"
-        }
-        sleep 5
-        grep_status_pr = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$PROXY_IP_ADDR:$APP_HTTP_PORT | grep 'Greetings, stranger!'").trim()
-        if (grep_status_pr != '<title>Greetings, stranger!</title>') {
-            currentBuild.result = 'FAILED'
-            sh "exit ${grep_status_pr}"
-        }
-        echo "Application started on http://$PROXY_IP_ADDR:$APP_HTTP_PORT"
+        try {
+            withCredentials([usernamePassword(credentialsId: 'postgresql-dmanev', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
+                sh "docker pull $DOCKER_HUB_USER/$CONTAINER_NAME:${env.git_tags}"
+                sh "docker run -d --network greetings_application_default  -p $APP_PORT:$APP_PORT -e 'DB_URL=postgresql://$USER:$PASSWORD@postgresql:$DB_PORT/$DB_NAME' --name $CONTAINER_NAME $DOCKER_HUB_USER/$CONTAINER_NAME:${env.git_tags}"
+            }
+            status = sh(returnStdout: true, script: "docker inspect --format='{{.State.Status}}' $CONTAINER_NAME").trim()
+            if (status != 'running'){
+                currentBuild.result = 'FAILED'
+                sh "exit 1"
+            }
+            sleep 5
+            APP_IP_ADDR = sh(returnStdout: true, script: "docker inspect $CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
+            curl_status_app = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_IP_ADDR:$APP_PORT 1>/dev/null && echo \$?")
+            if (curl_status_app != '0') {
+                currentBuild.result = 'FAILED'
+                sh "exit ${curl_status_app}"
+            }
+            sleep 5
+            grep_status_app = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_IP_ADDR:$APP_PORT | grep 'Greetings, stranger!'").trim()
+            if (grep_status_app != '<title>Greetings, stranger!</title>') {
+                currentBuild.result = 'FAILED'
+                sh "exit ${grep_status_app}"
+            }
+            sleep 5
+            PROXY_IP_ADDR = sh(returnStdout: true, script: "docker inspect $PROXY_CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
+            curl_status_pr = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$PROXY_IP_ADDR:$APP_HTTP_PORT 1>/dev/null && echo \$?")
+            sh(returnStatus: true, script: "echo http://$APP_IP_ADDR:$APP_PORT")
+            if (curl_status_pr != '0') {
+                currentBuild.result = 'FAILED'
+                sh "exit ${curl_status_pr}"
+            }
+            sleep 5
+            grep_status_pr = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$PROXY_IP_ADDR:$APP_HTTP_PORT | grep 'Greetings, stranger!'").trim()
+            if (grep_status_pr != '<title>Greetings, stranger!</title>') {
+                currentBuild.result = 'FAILED'
+                sh "exit ${grep_status_pr}"
+            }
+            echo "Application started on http://$PROXY_IP_ADDR:$APP_HTTP_PORT"
+        } catch (Exception err) {
+            currentBuild.result = 'SUCCESS'
+        }     
     }
-        
 }
 
