afa9db7e95ee266b77d5d2e17481be988af9f291
Dmitrii Manev <dmanevd@mail.ru>
2018-06-20 18:26:40 +0300

    1

--- jenkins/CD_job.groovy
+++ jenkins/CD_job.groovy
@@ -17,6 +17,7 @@ node {
             sh "docker image prune -f"
             sh "docker stop $CONTAINER_NAME"
             sh "docker rm $CONTAINER_NAME"
+            currentBuild.result = 'SUCCESS'
           } catch (Exception err) {
             currentBuild.result = 'FAILED'
         }
@@ -28,12 +29,18 @@ node {
                 sh "docker pull $DOCKER_HUB_USER/$CONTAINER_NAME:${env.git_tags}"
                 sh "docker run -d --network greetings_application_default  -p $APP_PORT:$APP_PORT -e 'DB_URL=postgresql://$USER:$PASSWORD@postgresql:$DB_PORT/$DB_NAME' --name $CONTAINER_NAME $DOCKER_HUB_USER/$CONTAINER_NAME:${env.git_tags}"
             }
-            status = sh(returnStdout: true, script: "docker inspect --format='{{.State.Status}}' $CONTAINER_NAME").trim()
+        try {            
+	    status = sh(returnStdout: true, script: "docker inspect --format='{{.State.Status}}' $CONTAINER_NAME").trim()
             if (status != 'running'){
                 currentBuild.result = 'FAILED'
                 sh "exit 1"
             }
             sleep 5
+            currentBuild.result = 'SUCCESS'
+        } catch (Exception err) {
+            currentBuild.result = 'FAILED'
+        }
+        try {
             APP_IP_ADDR = sh(returnStdout: true, script: "docker inspect $CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
             curl_status_app = sh(returnStdout: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_IP_ADDR:$APP_PORT 1>/dev/null && echo \$?")
             if (curl_status_app != '0') {
@@ -41,12 +48,22 @@ node {
                 sh "exit ${curl_status_app}"
             }
             sleep 5
+            currentBuild.result = 'SUCCESS'
+        } catch (Exception err) {
+            currentBuild.result = 'FAILED'
+        }
+        try {
             grep_status_app = sh(returnStdout: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$APP_IP_ADDR:$APP_PORT | grep 'Greetings, stranger!' 1>/dev/null && echo \$?").trim()
             if (grep_status_app != '0') {
                 currentBuild.result = 'FAILED'
                 sh "exit ${grep_status_app}"
             }
             sleep 5
+            currentBuild.result = 'SUCCESS'
+        } catch (Exception err) {
+            currentBuild.result = 'FAILED'
+        }
+        try {
             PROXY_IP_ADDR = sh(returnStdout: true, script: "docker inspect $PROXY_CONTAINER_NAME --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'").trim()
             curl_status_pr = sh(returnStatus: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$PROXY_IP_ADDR:$APP_HTTP_PORT 1>/dev/null && echo \$?")
             sh(returnStatus: true, script: "echo http://$APP_IP_ADDR:$APP_PORT")
@@ -55,6 +72,11 @@ node {
                 sh "exit ${curl_status_pr}"
             }
             sleep 5
+            currentBuild.result = 'SUCCESS'
+        } catch (Exception err) {
+            currentBuild.result = 'FAILED'
+        }
+        try {
             grep_status_pr = sh(returnStdout: true, script: "curl --silent --connect-timeout 15 --show-error --fail http://$PROXY_IP_ADDR:$APP_HTTP_PORT | grep 'Greetings, stranger!' 1>/dev/null && echo \$?").trim()
             if (grep_status_pr != '0') {
                 currentBuild.result = 'FAILED'
@@ -67,4 +89,3 @@ node {
         }     
     }
 }
-
